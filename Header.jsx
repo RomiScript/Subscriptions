df0353a63b7bee5876d2ea8c4735c335962a7634
Romina Martínez <romina.martinez496@gmail.com>
2025-06-08 14:15:36 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div className="header">
+            <h2>Subscriptions Tracker</h2>
+        </div>
+
+    );
+}
+
+export default Header;
---
+++ MainControl.jsx
@@ -0,0 +1,10 @@
+const MainControl = ({ count }) => {
+    return (
+        <div className="main-control">
+            <h4>{count}</h4>
+
+        </div>
+    );
+}
+
+export default MainControl;
